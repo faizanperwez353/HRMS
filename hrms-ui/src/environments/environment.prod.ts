36babf50bacd44ff82337543081c232ca90c6d5c
Faizan Perwez <faizanperwez12345@gmail.com>
2026-04-18 11:43:45 +0530

    update environment configuration for development and production

---
+++ hrms-ui/src/environments/environment.prod.ts
@@ -0,0 +1,4 @@
+export const environment = {
+    production: true,
+    apiUrl: 'https://hrms-1-z8jh.onrender.com/api'
+};
--- hrms-ui/src/environments/environment.ts
+++ hrms-ui/src/environments/environment.ts
@@ -1,5 +1,4 @@
 export const environment = {
     production: false,
-    // apiUrl: 'http://localhost:8080/api'
-     apiUrl: 'https://hrms-1-z8jh.onrender.com/api'
+    apiUrl: 'http://localhost:8080/api'
 };
