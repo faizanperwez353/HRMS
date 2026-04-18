d83ef09dba5b392452fb6d15146d75e71cb0127c
Faizan Perwez <faizanperwez12345@gmail.com>
2026-04-18 11:40:22 +0530

    changes in env

--- hrms-ui/src/environments/environment.ts
+++ hrms-ui/src/environments/environment.ts
@@ -1,4 +1,5 @@
 export const environment = {
     production: false,
-    apiUrl: 'http://localhost:8080/api'
+    // apiUrl: 'http://localhost:8080/api'
+     apiUrl: 'https://hrms-1-z8jh.onrender.com/api'
 };
